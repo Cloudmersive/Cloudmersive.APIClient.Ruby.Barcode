eb071c6f2467dad009fcad97f05c1118f8d5388d
Cloudmersive <35204726+Cloudmersive@users.noreply.github.com>
2025-11-02 21:49:07 +0000

    Added

--- build.ps1
+++ build.ps1
@@ -1,7 +1,13 @@
 ﻿Remove-Item –path ./client –recurse
 Remove-Item *.gem
 Remove-Item ./client/*.gem
-& java -jar swagger-codegen-cli-2.4.14.jar generate -i https://api.cloudmersive.com/swagger/api/barcode -l ruby -o client -c packageconfig.json
+
+
+Invoke-WebRequest -Uri 'https://api.cloudmersive.com/swagger/api/barcode' -OutFile '.\barcode-api-swagger.json'
+(Get-Content .\barcode-api-swagger.json).replace('localhost', "api.cloudmersive.com") | Set-Content .\barcode-api-swagger.json
+(Get-Content .\barcode-api-swagger.json -Raw) -replace '"http"','"https"' | Set-Content .\barcode-api-swagger.json -Encoding UTF8
+
+& java -jar swagger-codegen-cli-2.4.14.jar generate -i .\barcode-api-swagger.json -l ruby -o client -c packageconfig.json
 #(Get-Content ./client/src/api/ConvertDocumentApi.js).replace('var returnType = Object;', "var returnType = 'Blob';") | Set-Content ./client/src/api/ConvertDocumentApi.js
 #(Get-Content ./client/src/api/ConvertWebApi.js).replace('var returnType = Object;', "var returnType = 'Blob';") | Set-Content ./client/src/api/ConvertWebApi.js
 
